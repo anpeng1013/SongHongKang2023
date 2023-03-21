d7e01901830a319092e601647c4d08ef3c438a71
anpeng1013 <anpeng1013@163.com>
2023-03-21 14:49:17 +0800

    add KeyWord.java

--- src/day02_javaFoundation/a_KeyWord.java
+++ src/day02_javaFoundation/a_KeyWord.java
@@ -1,7 +1,36 @@
 package day02_javaFoundation;
 
+/**
+ *  定义：被 Java 语言赋予了特殊含义，用做专门用途的字符串（或单词），全部关键字都是小写字母。
+ *  说明：关键字一共50个，其中const和goto都是保留字(reserved word);
+ *       true,false,null不在其中，它们看起来像关键字，其实是字面量，表示特殊的布尔值和空值。
+ *  分类：
+ *      定义数据类型：
+ *          class    int       enum     byte      short     interface
+ *          long     float     double   char      boolean   void
+ *      定义流程控制：
+ *          if       else      switch   case      default   while
+ *          do       for       break    return    continue
+ *      定义访问权限：
+ *          public   private   protected
+ *      类函数及变量：
+ *          final    abstract  static   synchronized
+ *      定义类间关系：
+ *          extends  implements
+ *      创建引用实例：
+ *          new      this      super    instanceof
+ *      用于异常处理：
+ *          try      catch     finally  throw     throws
+ *      定义包的使用：
+ *          package  import
+ *      其他修饰：
+ *          native   strictfp  assert   volatile  transient
+ *      保留字：
+ *          const    goto
+ *
+ */
 public class a_KeyWord {
     public static void main(String[] args) {
-
+        System.out.println("this is key word description document.");
     }
 }
---
+++ src/day02_javaFoundation/b_Identifier.java
@@ -0,0 +1,15 @@
+package day02_javaFoundation;
+
+/**
+ * 标识符：Java 中变量、方法、类等要素命名时使用的字符序列，称为标识符。
+ * 命名规则：
+ *      1、由26个英文字母大小写，0-9，_或 $ 组成。
+ *      2、数字不可以开头。
+ *      3、不可以使用关键字和保留字，但能包含关键字和保留字。
+ *      4、Java 中严格区分大小写，长度无限制，但不能包含空格。
+ */
+public class b_Identifier {
+    public static void main(String[] args) {
+        System.out.println("this is a identifier description document.");
+    }
+}
