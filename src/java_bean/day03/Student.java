3a58efb3ac12f4165ed1ee368ed2a88c1a204658
anpeng1013 <anpeng1013@163.com>
2023-10-22 22:07:17 +0800

    add Student.java

---
+++ src/java_bean/day03/Student.java
@@ -0,0 +1,18 @@
+package java_bean.day03;
+
+/**
+ * @ClassName: Student.java
+ * @Author: anpeng
+ * @Date: 2023/10/22 22:05
+ */
+public class Student extends Person{
+    private String school;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+}
